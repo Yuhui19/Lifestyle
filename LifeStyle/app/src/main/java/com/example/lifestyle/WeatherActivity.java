6bd83f1cfa09f69aad11c54f51d798092f0091f2
qianhuiwei <u1041216@utan.edu>
2020-09-17 22:37:47 -0600

    fix profile photo bug and add WeatherActivity

---
+++ LifeStyle/app/src/main/java/com/example/lifestyle/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.lifestyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        setTitle("Weather");
+    }
+}
