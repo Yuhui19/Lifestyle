72f2388e284f7bf95af3d00f804ee77d89430fee
xiangjieli <xiangjieli1997@gmail.com>
2020-09-28 21:43:26 -0600

    add edit button listener

--- LifeStyle/app/src/main/java/com/example/lifestyle/ProfileFragment.java
+++ LifeStyle/app/src/main/java/com/example/lifestyle/ProfileFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+
+    private Button mbuttonEdit;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mbuttonEdit = (Button) view.findViewById(R.id.button_edit_profile);
+        mbuttonEdit.setOnClickListener(this);
         return view;
     }
 
@@ -22,4 +28,13 @@ public class ProfileFragment extends Fragment {
         //Event handling for view objects can go here
 
    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_edit_profile: {
+                
+            }
+        }
+    }
 }
