c533abbf90ccfdcfa45c182d077f2442b4f32049
qianhuiwei <u1041216@utan.edu>
2020-10-22 16:29:33 -0600

    add step counter UI

---
+++ LifeStyle/app/src/main/java/com/example/lifestyle/StepCounterFragment.java
@@ -0,0 +1,28 @@
+package com.example.lifestyle;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+
+public class StepCounterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step_counter, container, false);
+        return view;
+    }
+}
